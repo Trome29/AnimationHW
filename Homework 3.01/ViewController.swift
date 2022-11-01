39cbf5748f6584912ca98242cabac80bf0d4c983
Trome29 <108098922+Trome29@users.noreply.github.com>
2022-11-01 09:47:04 +0200

    Initial Commit

---
+++ Homework 3.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 3.01
+//
+//  Created by Roman on 01.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
